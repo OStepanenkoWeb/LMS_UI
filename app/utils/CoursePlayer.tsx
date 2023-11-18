52f0caf9de729cc43a2b42c41cae0e5971dd222f
Stepanenko Oleg <o.stepanenko@id-mt.ru>
2023-11-18 11:09:01 +0300

    added course preview component

---
+++ app/utils/CoursePlayer.tsx
@@ -0,0 +1,29 @@
+import React, {FC} from 'react';
+
+interface ICoursePlayer {
+    videoUrl: string,
+    title: string
+}
+
+const CoursePlayer:FC<ICoursePlayer> = ({ videoUrl, title}) => {
+    console.log('CoursePlayer', videoUrl) // https://www.youtube.com/embed/Big_aFLmekI
+    return (
+        <div style={{paddingTop: '41%', position: 'relative'}}>
+            <iframe
+                width="420"
+                height="315"
+                style={{
+                    border: 0,
+                    width: '90%',
+                    height: '100%',
+                    position: 'absolute',
+                    top: 0,
+                    left: 0
+                }}
+                src={videoUrl}>
+            </iframe>
+        </div>
+    );
+};
+
+export default CoursePlayer;
